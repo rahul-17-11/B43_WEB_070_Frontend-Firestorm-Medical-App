7d3ae47912ac3bac89a1d1275c9a566971e78791
Rahul Waghmare <rahulwaghmare1711@gmail.com>
2025-03-23 17:10:40 +0530

    logo added and ai response for user symptoms done

--- project/src/firebase/config.ts
+++ project/src/firebase/config.ts
@@ -1,13 +1,13 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBPua2dc3ON-JsKrnaf0jLonDvAb7xg0o",
-  authDomain: "medicalapp-8f065.firebaseapp.com",
-  projectId: "medicalapp-8f065",
-  storageBucket: "medicalapp-8f065.firebasestorage.app",
-  messagingSenderId: "599251902516",
-  appId: "1:599251902516:web:7ca3cb08734ddd7c656a61"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
